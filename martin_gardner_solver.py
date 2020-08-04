8e591fb364f534fe16df5e998368f61a648076f7
Giovanni Maio <gmaio@catalyte.io>
2020-08-04 13:42:03 -0700

    Add file to repo. Only handles two-digit numbers

---
+++ martin_gardner_solver.py
@@ -0,0 +1,43 @@
+'''
+A number's persistence is the number of steps required to reduce it to a single digit by multiplying all its digits to obtain a second number, 
+then multiplying all the digits of that number to obtain a third number, and so on until a one-digit number is obtained. 
+For example, 77 has a persistence of four because it requires four steps to reduce it to one digit: 77-49-36-18-8. 
+The smallest number of persistence one is 10, the smallest of persistence two is 25, the smallest of persistence three is 39, and the smaller of persistence four is 77. 
+What is the smallest number of persistence five?
+'''
+
+
+def multiplyDigits(number):
+    # Splits each digit in a given number individually into an array
+    digits = [int(d) for d in str(number)]
+    # Multiply the digits together
+    return digits[0] * digits[1]
+
+
+def findPersistence(number):
+    productList = []
+    # Multiply each digit in the number EX: 77 ---> 7*7
+    product = multiplyDigits(number)
+    print("Current Number: " + str(product))
+    productList.append(product)
+
+    while len(str(product)) != 1:
+        product = multiplyDigits(product)
+        print("Current Number: " + str(product))
+        productList.append(product)
+
+    persistence = len(productList)
+    print("Done")
+    print("Persistence length: " + str(persistence))
+    return persistence
+
+
+def solve():
+    for x in range(10, 99):
+        fivePersistenceNumbers = []
+        if findPersistence(x) == 5:
+            fivePersistenceNumbers.append(x)
+    print(fivePersistenceNumbers)
+
+
+solve()
